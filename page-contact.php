d15a430c266168b828b483ec476b47a0245fff95
ChrisBAshton <chrisashtonweb@gmail.com>
2017-09-03 15:17:11 +0100

    added contact template

---
+++ page-contact.php
@@ -0,0 +1,14 @@
+<?php get_header(); ?>
+
+<h1>Contact</h1>
+
+<p>
+    My email address is my first name @ the name of this website.
+</p>
+
+<h2>Alternative Contact Methods</h2>
+
+<a href="<?= ChrisApi::linkedin(); ?>" target="_blank">LinkedIn</a> - <a href="<?= ChrisApi::twitter(); ?>" target="_blank">Twitter</a> - <a href="<?= ChrisApi::github(); ?>" target="_blank">GitHub</a> - <a href="skype:chrisbashton?userinfo">Skype</a>
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
